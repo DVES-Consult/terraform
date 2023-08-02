2eb5cb61ab9875b864057445eeb38f2c08a9437e
Henrique Dias Aguiar <hrikelr@hotmail.com>
2023-08-02 16:02:38 -0300

    Update scripts-front.sh

--- auto-scaling/infrastructure/scripts-front.sh
+++ auto-scaling/infrastructure/scripts-front.sh
@@ -2,4 +2,4 @@
 sudo apt-get update
 sudo apt-get -y install docker.io
 sudo usermod -aG docker ubuntu
-sudo docker run -d -p 80:80 henriquelr/meu-site-nginx
+sudo docker run -d -p 80:80 henriquelr/meu-site-nginx:latest
